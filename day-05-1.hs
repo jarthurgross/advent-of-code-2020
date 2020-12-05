a276418bbfa3c8c54f329d8a6780218c6a795fd8
Jonathan A. Gross <azaghalofbelegost@gmail.com>
2020-12-04 23:47:27 -0700

    Cleanup part 1 of day 5 a bit

--- day-05-1.hs
+++ day-05-1.hs
@@ -2,7 +2,15 @@ import Data.List.Split
 
 main = do
   contents <- getContents
-  putStrLn $ show $ maximum $ map (\(a, b) -> 8 * (getRow a) + (getCol b)) $ map splitRowCol $ lines contents
+  putStrLn $ show $ maximum $ map (\(a, b) -> 8 * (getRow' a) + (getCol' b)) $ map (splitAt 7) $ lines contents
+
+getRow' :: String -> Int
+getRow' s = sum $ zipWith (*) (iterate (*2) 1) $ map (\c -> if c == 'F' then 0 else 1) $ reverse s
+
+getCol' :: String -> Int
+getCol' s = sum $ zipWith (*) (iterate (*2) 1) $ map (\c -> if c == 'L' then 0 else 1) $ reverse s
+
+-- Original versions of functions
 
 splitRowCol :: String -> (String, String)
 splitRowCol s = (take 7 s, reverse $ take 3 $ reverse s)
