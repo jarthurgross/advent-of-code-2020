520d295a5a8108e23903edb9e17c3e36b2236973
Jonathan A. Gross <azaghalofbelegost@gmail.com>
2020-12-16 17:17:43 -0700

    Add solutions for day 13

--- day-13-1.hs
+++ day-13-1.hs
@@ -9,7 +9,21 @@ import Math.Sequence.Converge
 
 main = do
   contents <- getContents
-  putStrLn $ show $ lines contents
+  putStrLn $ show $ getProduct $ parseInput $ lines contents
+
+parseInput :: [String] -> (Int, [Int])
+parseInput [] = (0, [])
+parseInput (x:[]) = (readInt x, [])
+parseInput (x:y:_) = (readInt x, map readInt $ filter (/= "x") $ splitOn "," y)
+
+minutesToNext :: Int -> Int -> Int
+minutesToNext t f = (- (t `mod` f)) `mod` f
+
+getWaitTimes :: (Int, [Int]) -> [Int]
+getWaitTimes (t, fs) = map (minutesToNext t) fs
+
+getProduct :: (Int, [Int]) -> Int
+getProduct (t, fs) = (uncurry (*)) $ minimum $ zip (getWaitTimes (t, fs)) fs
 
 readInt :: String -> Int
 readInt = read
--- day-13-2.hs
+++ day-13-2.hs
@@ -9,7 +9,24 @@ import Math.Sequence.Converge
 
 main = do
   contents <- getContents
-  putStrLn $ show $ lines contents
+  putStrLn $ show $ fst $ foldl' sieve (0, 1) $ sortByModulus $ map getRemainder $ parseInput $ lines contents
+
+parseInput :: [String] -> [(Int, Int)]
+parseInput [] = []
+parseInput (_:[]) = []
+parseInput (_:x:_) = reverse $ snd $ foldr (\s (n, ss) -> (n + 1, if s /= "x" then (n, readInt s):ss else ss)) (0, []) $ reverse $ splitOn "," x
+
+getRemainder :: (Int, Int) -> (Int, Int)
+getRemainder (x, y) = ((-x) `mod` y, y)
+
+swap :: (a, b) -> (b, a)
+swap (x, y) = (y, x)
+
+sortByModulus :: Ord a => Ord b => [(a, b)] -> [(a, b)]
+sortByModulus = (map swap) . reverse . sort . (map swap)
+
+sieve :: (Int, Int) -> (Int, Int) -> (Int, Int)
+sieve (a1, n1) (a2, n2) = (head $ dropWhile (\x -> (x `mod` n2 /= a2)) [a1, a1 + n1 ..], n1 * n2)
 
 readInt :: String -> Int
 readInt = read
