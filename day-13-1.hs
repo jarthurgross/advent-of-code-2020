40b222bcbfad86c72f2945d15e00d9613b74af57
Jonathan A. Gross <azaghalofbelegost@gmail.com>
2020-12-16 17:27:44 -0700

    Remove unnecessary code from day 13

--- day-13-1.hs
+++ day-13-1.hs
@@ -27,22 +27,3 @@ getProduct (t, fs) = (uncurry (*)) $ minimum $ zip (getWaitTimes (t, fs)) fs
 
 readInt :: String -> Int
 readInt = read
-
-takeDigits :: String -> (String, String)
-takeDigits "" = ("", "")
-takeDigits (c:cs) = if c `elem` ['0' .. '9'] then (c:ds, ls) else ("", c:cs)
-  where (ds, ls) = takeDigits cs
-
-count :: Eq a => a -> [a] -> Int
-count c = foldr (\c' n -> if c == c' then n + 1 else n) 0
-
-mapCount :: Eq a => a -> Map k a -> Int
-mapCount c = Map.foldr (\c' n -> if c == c' then n + 1 else n) 0
-
-stride :: Int -> [a] -> [a]
-stride n = (map head) . (chunksOf n)
-
-xor :: Bool -> Bool -> Bool
-xor True False = True
-xor False True = True
-xor _ _ = False
--- day-13-2.hs
+++ day-13-2.hs
@@ -30,22 +30,3 @@ sieve (a1, n1) (a2, n2) = (head $ dropWhile (\x -> (x `mod` n2 /= a2)) [a1, a1 +
 
 readInt :: String -> Int
 readInt = read
-
-takeDigits :: String -> (String, String)
-takeDigits "" = ("", "")
-takeDigits (c:cs) = if c `elem` ['0' .. '9'] then (c:ds, ls) else ("", c:cs)
-  where (ds, ls) = takeDigits cs
-
-count :: Eq a => a -> [a] -> Int
-count c = foldr (\c' n -> if c == c' then n + 1 else n) 0
-
-mapCount :: Eq a => a -> Map k a -> Int
-mapCount c = Map.foldr (\c' n -> if c == c' then n + 1 else n) 0
-
-stride :: Int -> [a] -> [a]
-stride n = (map head) . (chunksOf n)
-
-xor :: Bool -> Bool -> Bool
-xor True False = True
-xor False True = True
-xor _ _ = False
